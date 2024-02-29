7489e46abe7dbb3285c5ca4c333e1e43864cd47b
mscno <msc@parap.ly>
2024-02-29 15:48:21 +0100

    fmt

--- routes/api/parse.ts
+++ routes/api/parse.ts
@@ -1,11 +1,15 @@
 import { createCompoundSchema, createSchema } from "npm:genson-js@0.0.8";
 
 export const handler = async (req: Request): Promise<Response> => {
-  if (req.method !== "POST") return new Response("Method Not Allowed", { status: 405 });
+  if (req.method !== "POST") {
+    return new Response("Method Not Allowed", { status: 405 });
+  }
 
   try {
     const body = await req.json();
-    const schema = Array.isArray(body) ? createCompoundSchema(body) : createSchema(body);
+    const schema = Array.isArray(body)
+      ? createCompoundSchema(body)
+      : createSchema(body);
     return Response.json(schema);
   } catch (error) {
     return new Response("Bad Request: " + error.toString(), { status: 400 });
